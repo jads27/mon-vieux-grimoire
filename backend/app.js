fbee934c0f771f1e10522ceb371a02d2aee3bb09
jads27 <jalvesdsilva27@gmail.com>
2023-10-07 17:54:15 +0200

    Add app configuration + Init and add packages

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+    .connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+});
+
+module.exports = app;
