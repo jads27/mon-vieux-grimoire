3955871553d16e48a13a5ef6ca5d332b68138eb2
jads27 <jalvesdsilva27@gmail.com>
2023-10-09 10:05:07 +0200

    Set authentification routes and controllers

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const userRoutes = require("./routes/user");
+
 mongoose
     .connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connexion à MongoDB réussie !"))
@@ -17,5 +19,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
 });
+
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
