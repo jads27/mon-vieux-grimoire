6013dbded578d3ca2c54d842723d7e7b3a1da8bc
jads27 <jalvesdsilva27@gmail.com>
2023-10-12 21:28:11 +0200

    Set rateBook and getBestRatedBooks

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -56,7 +56,7 @@ exports.deleteBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
         .then((book) => {
             if (book.userId != req.auth.userId) {
-                res.status(400).json({ message: "Non autorisé" });
+                res.status(403).json({ message: "Non autorisé" });
             } else {
                 const filename = book.imageUrl.split("/images/")[1];
                 fs.unlink(`images/"${filename}`, () => {
@@ -66,5 +66,45 @@ exports.deleteBook = (req, res, next) => {
                 });
             }
         })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+exports.rateBook = (req, res, next) => {
+    const userId = req.body.userId;
+    const rating = req.body.rating;
+    console.log(req.body);
+
+    Book.findById(req.params.id)
+        .then((book) => {
+            console.log(book);
+            if (!book) {
+                return res.status(404).json({ message: "Livre non trouvé" });
+            }
+
+            if (book.ratings.find((rating) => rating.userId === userId)) {
+                return res.status(400).json({ message: "L'utilisateur a déjà noté ce livre." });
+            } else {
+                book.ratings.push({
+                    userId: userId,
+                    grade: rating,
+                });
+
+                const totalRatings = book.ratings.length;
+                const sumOfRatings = book.ratings.reduce((sum, rating) => sum + rating.grade, 0);
+                book.averageRating = sumOfRatings / totalRatings;
+                
+                book.save()
+                    .then((book) => res.status(200).json(book))
+                    .catch((error) => res.status(400).json({ error }));
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+exports.getBestRatedBooks = (req, res, next) => {
+    Book.find()
+        .sort({ averageRating: -1 })
+        .limit(3)
+        .then((books) => res.status(200).json(books))
         .catch((error) => res.status(500).json({ error }));
 };
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,10 +7,10 @@ const bookCtrl = require("../controllers/book");
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getOneBook);
-router.get("/bestrating");
+router.get("/bestrating", bookCtrl.getBestRatedBooks);
 router.post("/", auth, multer, bookCtrl.createBook);
 router.put("/:id", auth, multer, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
-router.post("/:id/rating");
+router.post("/:id/rating", auth, bookCtrl.rateBook);
 
 module.exports = router;
